3b2d6d3f66676696321f01d57d583a6c34b218f6
serg-n3 <ktg80@yandex.ru>
2023-10-07 17:42:44 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,20 @@
+package concurrent;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("первый поток " + first.getState());
+            System.out.println("второй поток " + second.getState());
+        }
+        System.out.println("работа завершена");
+    }
+}
