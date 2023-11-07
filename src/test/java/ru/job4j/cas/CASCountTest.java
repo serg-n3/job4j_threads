57fce3771adab15828459e9db7d6960a82520606
serg-n3 <ktg80@yandex.ru>
2023-11-07 13:47:25 +0300

    0. CAS - операции [#6859]

--- src/test/java/ru/job4j/cas/CASCountTest.java
+++ src/test/java/ru/job4j/cas/CASCountTest.java
@@ -24,4 +24,24 @@ class CASCountTest {
 
     }
 
+    @Test
+    void whenTwoTreads() throws InterruptedException {
+        CASCount casCount = new CASCount();
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                casCount.increment();
+            }
+        });
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                casCount.increment();
+            }
+        });
+        thread.start();
+        thread1.start();
+        thread.join();
+        thread1.join();
+        assertThat(casCount.get()).isEqualTo(6);
+    }
+
 }
